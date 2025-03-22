669a45448bc85343726bcba9a10d7a82aa55f630
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-03-22 15:13:09 +0100

    getbyid + mapuser

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,10 +1,15 @@
+import type { IUser } from '@/types/interfaces/IUser';
 import instance from './httpClient'
-//todo: add missing imports for interfaces, types
 
 export class UserService {
     async login(email: String, password: String) {
-        const response = await instance.post('/auth/login', { email, password });
-        return response.data;
+        try {
+            const response = await instance.post('/auth/login', { email, password });
+            return response.data;
+        } catch (error) {
+            console.error('Error during login:', error);
+            throw error;
+        }
     }
     
     async register(){
@@ -12,5 +17,31 @@ export class UserService {
     
     async logout() {
         //endpoint for logout or just clear token + loggedInUser info ?
+    }
+
+    async getUserById(userId: string) {
+      try {
+        const response = await instance.get(`/user/${userId}`);
+        return this.mapToIUser(response.data);
+      } catch (error) {
+        console.error('Error fetching user by id:', error);
+        throw error;
+      }
+    }
+
+    mapToIUser(user: any): IUser {
+      return {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        bio: user.bio,
+        bookshelves: user.bookshelves,
+        readingProgress: user.readingProgress,
+        followers: user.followers,
+        following: user.following,
+        readingStats: user.readingStats,
+        createdAt: user.createdAt,
+      };
     }
 }
