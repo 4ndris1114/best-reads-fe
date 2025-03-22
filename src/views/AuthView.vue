d965a15f25b69cdda2c5f8f3752128207159cde9
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-03-22 16:02:49 +0100

    created register form

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="min-h-screen bg-gray-50 flex items-center justify-center px-4">
-        <LoginForm />
+        <LoginForm v-if="!isRegistering" @signup="isRegistering = true" />
+        <RegisterForm v-if="isRegistering" @signin="isRegistering = false" />
     </div>
 </template>
 
 <script setup lang="ts">
 import LoginForm from '@/components/LoginForm.vue';
+import RegisterForm from '@/components/RegisterForm.vue';
+import { ref } from 'vue';
+
+const isRegistering = ref(
+    new URLSearchParams(window.location.search).get('isRegistering') === 'true'
+);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
