31f6d2b3511139970e0009e697fe11e94a761006
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-03-22 17:57:44 +0100

    Login and register page (#2)
    * Create LoginForm.vue
    
    * Create AuthView.vue
    
    * added route for /login
    
    * userservice login method
    
    * added login and token handling at user store
    
    * create IUser IF
    
    * getbyid + mapuser
    
    * loggedInUser + getById
    
    * created user related interfaces
    
    * dunno what this is
    
    * login form error handling
    
    * added IsRegistering query string logic
    
    * created register form
    
    * register methods at store and service
    
    * initAuth method upon loading (assigns loggedInUser)
    
    * error handling from backend
    
    * deleted counter.ts
    
    * set up navigation guards
    
    * reset error messages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useUserStore } from '../stores/userStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,28 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      beforeEnter: async (to, from, next) => {
+        const userStore = useUserStore()
+        if (!userStore.isAuthenticated) {
+          next({ name: 'auth' })
+        } else {
+          next()
+        }
+      },
     },
+    {
+      path: '/login',
+      name: 'auth',
+      component: () => import('../views/AuthView.vue'),
+      beforeEnter: async (to, from, next) => {
+        const userStore = useUserStore()
+        if (userStore.isAuthenticated) {
+          next({ name: 'home' })
+        } else {
+          next()
+        }
+      },
+    }
     // {
     //   path: '/about',
     //   name: 'about',
@@ -21,3 +43,4 @@ const router = createRouter({
 })
 
 export default router
+
---
+++ src/types/interfaces/IUser.ts
@@ -0,0 +1,17 @@
+import type { IBookshelf } from "./IBookShelf";
+import type { IReadingProgress } from "./IReadingProgress";
+import type { IReadingStats } from "./IReadingstats";
+
+export interface IUser {
+    id: string;
+    username: string;
+    email: string;
+    profilePicture: string;
+    bio: string;
+    bookshelves: IBookshelf[];
+    readingProgress: IReadingProgress[];
+    followers: string[];
+    following: string[];
+    readingStats: IReadingStats;
+    createdAt: Date;
+}
