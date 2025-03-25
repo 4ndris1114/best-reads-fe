3008de2ad1329ee021abc54e195426dc682c83e4
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-03-25 12:22:37 +0100

    added onclickoutside

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -3,11 +3,11 @@
     <div class="flex-1 max-w-2xl mx-8 relative">
         <!-- Search bar -->
         <fa icon="search" class="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 text-sm" />
-        <input @click="toggleRecommendations()" type="text" v-model="searchQuery" placeholder="Search books, authors, or genres..."
+        <input @click="toggleRecommendations" type="text" v-model="searchQuery" placeholder="Search books, authors, or genres..."
             class="w-full px-4 py-2 pl-10 border-none bg-gray-100 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
         
         <!-- Search recommendations -->
-        <div @click.self="toggleRecommendations()" v-if="searchResults.length > 0 && isRecommendationsOpen" >
+        <div v-on-click-outside="toggleRecommendations" v-if="searchResults.length > 0 && isRecommendationsOpen" >
         <ul
             class="absolute w-full mt-2 bg-white shadow-lg rounded-lg overflow-hidden z-50">
             <li v-for="result in searchResults" :key="result.id" 
@@ -23,7 +23,7 @@
 import { useBookStore } from '@/stores/bookStore';
 import type { IBook } from '@/types/interfaces/IBook';
 import { computed, onMounted, ref } from 'vue';
-
+import { vOnClickOutside } from '@vueuse/components'
 onMounted(async () => {
     bookStore.getAllBooks();
 })
@@ -46,8 +46,6 @@ const searchResults = computed<IBook[]>(() => {
 });
 
 const toggleRecommendations = () => {
-    console.log(isRecommendationsOpen.value);
-    
     isRecommendationsOpen.value = !isRecommendationsOpen.value;
 };
 </script>
