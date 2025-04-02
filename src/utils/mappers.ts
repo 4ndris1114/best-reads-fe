16b18553ea2a59cb8c59924c6af0c9c867f29114
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-04-02 14:00:46 +0200

    ratings get mapped to objects on book

--- src/utils/mappers.ts
+++ src/utils/mappers.ts
@@ -1,42 +1,53 @@
 import type { IBook } from "@/types/interfaces/IBook";
 import type { IBookshelf } from "@/types/interfaces/IBookshelf";
 import type { IUser } from "@/types/interfaces/IUser";
+import type { IRating } from "@/types/interfaces/IRating";
 
 export const mapToIUser = (user: any): IUser => {
-    return {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      profilePicture: user.profilePicture,
-      bio: user.bio,
-      bookshelves: user.bookshelves.map((bookshelf: any) => mapToIBookshelf(bookshelf)) as IBookshelf[],
-      readingProgress: user.readingProgress,
-      followers: user.followers,
-      following: user.following,
-      readingStats: user.readingStats,
-      createdAt: user.createdAt,
-    } as IUser;
-  }
+  return {
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    profilePicture: user.profilePicture,
+    bio: user.bio,
+    bookshelves: user.bookshelves.map((bookshelf: any) => mapToIBookshelf(bookshelf)),
+    readingProgress: user.readingProgress,
+    followers: user.followers,
+    following: user.following,
+    readingStats: user.readingStats,
+    createdAt: user.createdAt,
+  } as IUser;
+}
 
-  export const mapToIBookshelf = (bookshelf: any): IBookshelf => {
-    return {
-      id: bookshelf.id,
-      name: bookshelf.name,
-      books: bookshelf.books,
-    } as IBookshelf;
-  }
+export const mapToIBookshelf = (bookshelf: any): IBookshelf => {
+  return {
+    id: bookshelf.id,
+    name: bookshelf.name,
+    books: bookshelf.books,
+  } as IBookshelf;
+}
 
- export const mapToIBook = (book: any): IBook => {
-    return {
-      id: book._id,
-      apiId: book.apiId,
-      title: book.title,
-      author: book.author,
-      isbn: book.isbn,
-      publishedDate: book.publishedDate,
-      coverImage: book.coverImage,
-      genres: book.genres,
-      ratings: book.ratings,
-      averageRating: book.averageRating
-    } as IBook;
+export const mapToIBook = (book: any): IBook => {
+  return {
+    id: book._id,
+    apiId: book.apiId,
+    title: book.title,
+    author: book.author,
+    isbn: book.isbn,
+    publishedDate: book.publishedDate,
+    coverImage: book.coverImage,
+    genres: book.genres,
+    ratings: book.ratings.map((rating: any) => mapToIRating(rating)),
+    averageRating: book.averageRating
+  } as IBook;
+}
+
+export const mapToIRating = (rating: any): IRating => {
+  return {
+    userId: rating.userId,
+    ratingValue: rating.ratingValue,
+    review: rating.rating,
+    createdAt: rating.createdAt,
+    updatedAt: rating.updatedAt
+  } as IRating;
 }
