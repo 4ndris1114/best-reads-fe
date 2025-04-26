8aad7909764fd4e5afd3a6fbddb1e210104f6df7
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-04-26 15:05:30 +0200

    simple activity components

---
+++ src/components/activity/AddedBookActivity.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-4 rounded-xl bg-gray-100">
+      <p><strong>{{ activity.userName }}</strong> added <strong>{{ activity.payload.bookTitle }}</strong> to their bookshelf.</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const props = defineProps<{ activity: any }>();
+  </script>
+  
---
+++ src/components/activity/RatedBookActivity.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-4 rounded-xl bg-gray-100">
+      <p><strong>{{ activity.userName }}</strong> rated <strong>{{ activity.payload.bookTitle }}</strong> as <strong>{{ activity.payload.rating }} stars</strong>.</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const props = defineProps<{ activity: any }>();
+  </script>
+  
