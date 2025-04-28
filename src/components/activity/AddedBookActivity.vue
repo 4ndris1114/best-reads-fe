aa763c4be97babf0210c6f901f2811511f6da824
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-04-28 13:28:44 +0200

    design activity comp. style

--- src/components/activity/AddedBookActivity.vue
+++ src/components/activity/AddedBookActivity.vue
@@ -1,23 +1,24 @@
 <template>
-    <div v-if="user" class="p-6 rounded-2xl bg-[#181C20] shadow-md border border-gray-700 text-white">
+    <div v-if="user" class="p-6 rounded-2xl bg-gray-200 shadow-md border border-gray-700 text-white">
         <div class="flex items-start space-x-4">
             <div class="flex-shrink-0">
                 <div>
                     <CloudinaryImage v-if="user.profilePicture !== 'default_profile_picture.jpg'" :publicId="user.profilePicture" alt="User profile image" :width="50" :height="50" />
-                    <img v-else src="@/assets/default_profile_picture.jpg" alt="User profile image" class="w-16 h-16 rounded-full text-black object-cover border" />
+                    <img v-else src="@/assets/default_profile_picture.jpg" alt="User profile image" class="w-20 h-20 rounded-full text-inherit object-cover border" />
                 </div>
             </div>
             <div>
-                <p class="text-sm text-gray-400">{{ new Date(activity.createdAt).toLocaleDateString() }}</p>
+                <p class="text-sm text-gray-500">{{ new Date(activity.createdAt).toLocaleDateString() }} at {{ new Date(activity.createdAt).toLocaleTimeString() }}</p>
                 <p class="mt-1">
-                    <span class="font-semibold text-white">{{ user.username }}</span>
-                    <span class="text-gray-400"> added </span>
-                    <span class="font-semibold text-blue-400">"{{ activity.payload.BookTitle }}"</span>
-                    <span class="text-gray-400"> to their </span>
-                    <span class="font-semibold text-purple-400">"{{ activity.payload.ShelfName }}"</span>
-                    <span class="text-gray-400"> bookshelf.</span>
+                    <span class="font-bold text-black">{{ user.username }}</span>
+                    <span class="text-gray-700"> added </span>
+                    <span class="font-semibold text-accent">"{{ activity.payload.BookTitle }}"</span>
+                    <span class="text-gray-700"> to their </span>
+                    <span class="font-semibold text-accent">"{{ activity.payload.ShelfName }}"</span>
+                    <span class="text-gray-700"> bookshelf.</span>
                 </p>
             </div>
+            <CloudinaryImage v-if="activity.payload.CoverImage" :publicId="activity.payload.CoverImage" alt="Book cover" :width="120" :height="180" />
         </div>
     </div>
 </template>
