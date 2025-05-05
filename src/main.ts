9b61f3e7c556fc8407b012fb2b1142f63fddf899
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-05-05 19:48:20 +0200

    added signalr connection

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,31 @@ import { createPinia } from 'pinia'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import activityHub from '@/utils/activityHub'; // Import SignalR connection
 
 import App from './App.vue'
 import router from './router'
 
 import { useUserStore } from '@/stores/userStore';
+import { useActivityStore } from './stores/activityStore'
 
 library.add(fas)
 const app = createApp(App).component('fa', FontAwesomeIcon)
+
+// Start the SignalR connection
+activityHub.start()
+  .then(() => {
+    console.log('SignalR connected!');
+  })
+  .catch((err) => {
+    console.error('SignalR connection failed:', err);
+  });
+
+// You can also listen for incoming events here (optional)
+activityHub.on('ReceiveActivity', (activity) => {
+    const activityStore = useActivityStore();
+    console.log('Received activity:', activity);
+});
 
 app.use(createPinia())
 
