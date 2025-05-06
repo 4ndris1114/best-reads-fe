b3543e8876d7dc9fff84591d4b5d779effb85893
Katarína Brezániová <katkabrezani@gmail.com>
2025-05-06 16:21:36 +0200

    progress bar

---
+++ src/components/ReadingProgressItem.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  {{ readingProgress.currentPage }}
+  <button @click="isEditProgressModalOpen = true">Update</button>
+  <EditProgressModal v-if="isEditProgressModalOpen" :readingProgress="readingProgress" />
+</div>
+</template>
+
+<script lang="ts" setup>
+import type { IReadingProgress } from '@/types/interfaces/IReadingProgress';
+import { ref } from 'vue';
+import EditProgressModal from './EditProgressModal.vue';
+
+const isEditProgressModalOpen = ref(false);
+const props = defineProps<{
+  readingProgress: IReadingProgress;
+}>();
+
+</script>
+<style scoped></style>
