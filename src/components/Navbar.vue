2c6fd0454d37ae4cd6dbba0e6c9b18ef0d3a9bd2
Katarína Brezániová <katkabrezani@gmail.com>
2025-05-07 14:38:48 +0200

    added logo to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,13 @@
     <!-- Header -->
     <header class="fixed top-0 w-full bg-accent text-white shadow-sm z-50">
       <div class="max-w-7xl mx-auto px-4 h-16 flex items-center justify-between">
-            <router-link to="/" class="text-3xl font-bold text-white-600 hover:text-slate-800">BestReads</router-link>
+        <router-link to="/" class="flex items-center">
+          <img src="@/assets/logo.png" alt="BestReads Logo" class="h-13 w-auto mr-2">
+          <div class="flex flex-col hover:text-slate-800 leading-tight">
+          <span class="text-3xl font-bold text-white-600">BestReads</span>
+          <span class="text-[9.5px] text-white-600 ">STORIES THAT TAKE YOU BEYOND</span>
+          </div>
+        </router-link>
 
            <!-- Navigation -->
           <nav class="flex items-center gap-6 mt-2 ml-8"><router-link to="/" class="text-white-700 hover:text-slate-800 font-bold text-xl ">Home</router-link>
