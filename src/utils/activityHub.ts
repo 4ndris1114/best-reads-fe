dc3ef629f210e9d62f095876016dfac2aea97928
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-05-15 01:59:55 +0200

    signalr uses render

--- src/utils/activityHub.ts
+++ src/utils/activityHub.ts
@@ -1,7 +1,7 @@
 import * as signalR from '@microsoft/signalr';
 
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl('http://localhost:5001/hubs/activity', {
+  .withUrl('https://best-reads-be.onrender.com/hubs/activity', {
     accessTokenFactory: () => localStorage.getItem('token') || ''
   })
   .withAutomaticReconnect()
